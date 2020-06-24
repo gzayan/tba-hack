bc296d8b5fc5eac8b009dd7550351f045fe3a87a
gzayan <gzayan@umich.edu>
2020-06-24 13:25:38 -0400

    organized views folder, set up routing

---
+++ src/app/views/donate/donate.component.js
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donate',
+  templateUrl: './donate.component.html',
+  styleUrls: ['./donate.component.css']
+})
+export class DonateViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
