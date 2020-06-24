7ba63cde2d5da90d81a6f819179181d8a5d90fc9
gzayan <gzayan@umich.edu>
2020-06-24 16:14:41 -0400

    added views folder for each page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { LoginViewComponent } from './views/login/login.component';
+import { HomeViewComponent } from './views/home/home.component';
+import { TransferViewComponent } from './views/transfer/transfer.component';
+import { DonateViewComponent } from './views/donate/donate.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [ 
+    LoginViewComponent, HomeViewComponent, TransferViewComponent, DonateViewComponent
+  ],
+  imports: [
+    RouterModule.forRoot([
+      { path: 'login', component: LoginViewComponent },
+      { path: 'home', component: HomeViewComponent },
+      { path: 'transfer', component: TransferViewComponent },
+      { path: 'donate', component: DonateViewComponent },
+      { path: '**', redirectTo: 'login' }
+    ])
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: [],
+
 })
 export class AppRoutingModule { }
