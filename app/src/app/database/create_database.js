5b8f6d80b1fedefc1705b4ed868f51409a650800
Apurv Goel <apurvgoel2304@gmail.com>
2020-07-01 11:33:45 -0700

    Connection credentials changed for database

--- app/src/app/database/create_database.js
+++ app/src/app/database/create_database.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql');
+const fs = require('fs');
 
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "sqldatabase",
-    port: 3306
+    host: 'db-mysql-tbahack-do-user-7692454-0.a.db.ondigitalocean.com',
+    user: 'doadmin',
+    password: 'vhm7v4y5awsdlgds',
+    port: 25060,
+    database: 'defaultdb',
+    ssl: {
+        rejectUnauthorized : false,
+        cert : fs.readFileSync('./ca-certificate.crt')
+    }
+
+    
 });
 
 conn.connect((err) => {
@@ -13,19 +21,4 @@ conn.connect((err) => {
     }
     console.log("Connected to the mysql server!");
 
-    let queryCreateDatabase = "CREATE DATABASE IF NOT EXISTS charityDb;";
-
-    conn.query(queryCreateDatabase, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log("Database create successfully !")
-    })
-
-    conn.end((err) => {
-        if (err) {
-            throw err;
-        }
-        console.log("Connection ended");
-    })
 });
--- app/src/app/database/database.js
+++ app/src/app/database/database.js
@@ -1,11 +1,17 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'sqldatabase',
-    database: 'charityDb',
-    port: 3306
+    // host: 'localhost',
+    // user: 'root',
+    // password: 'sqldatabase',
+    // database: 'charityDb',
+    // port: 3306
+    
+    host: 'db-mysql-tbahack-do-user-7692454-0.a.db.ondigitalocean.com',
+    user: 'doadmin',
+    password: 'vhm7v4y5awsdlgds',
+    port: 25060,
+    database: 'defaultdb'
 });
 
 module.exports = pool;
